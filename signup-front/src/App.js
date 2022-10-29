1fec1adce625fe7199d20e999b187c0620497318
Hien-Minh Le <minkdono@gmail.com>
2022-10-29 18:15:07 -0400

    fixed signup front

--- signup-front/src/App.js
+++ signup-front/src/App.js
@@ -1,16 +1,72 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <form>
-      <label>
-        Name:
-        <input type="text" name="name" />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      fullName: "",
+      email: "",
+      password: "",
+    };
+  }
+  changeFullName(event) {
+    this.setState({
+      fullName: event.target.value,
+    });
+  }
+  changeEmail(event) {
+    this.setState({
+      email: event.target.value,
+    });
+  }
+  changePassword(event) {
+    this.setState({
+      password: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="form-div">
+            <form action="">
+              <input
+                type="text"
+                placeholder="Full Name"
+                onChange={this.changeFullName}
+                value={this.state.fullName}
+                className="form-control form-group"
+              />
+
+              <input
+                type="text"
+                placeholder="E-mail"
+                onChange={this.changeEmail}
+                value={this.state.email}
+                className="form-control form-group"
+              />
+
+              <input
+                type="text"
+                placeholder="Password"
+                onChange={this.changePassword}
+                value={this.state.password}
+                className="form-control form-group"
+              />
+
+              <input
+                type="submit"
+                className="btn btn-danger btn-block"
+                value="Submit"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
