040f09775bdfcead565bf89849b420a143ccd30d
Hien-Minh Le <minkdono@gmail.com>
2022-10-31 10:53:36 -0400

    login front, signup both ends

--- signup-front/signup-back/server.js
+++ signup-front/signup-back/server.js
@@ -1,16 +1,43 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const routesUrls = require('./routes/routes')
+//const routesUrls = require('./routes/routes')
 const cors = require('cors')
+const passport = require("passport/lib");
+const users = require("./routes/api/users");
+const bodyParser = require("body-parser");
 
 require('dotenv').config();
 
-mongoose.connect(process.env.DATABASE_ACCESS,  () => console.log("Database connected"))
+// Connect to MongoDB
+mongoose
+  .connect(
+    process.env.DATABASE_ACCESS,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
 
-app.use(express.json()) //body parser
+// Bodyparser middleware
+app.use(
+    bodyParser.urlencoded({
+      extended: false
+    })
+  );
+  app.use(bodyParser.json());
+
 app.use(cors())
-app.use('/app', routesUrls) //appended to /app 
-app.listen(4000, () => console.log("server is up and running"))
+//app.use('/app', routesUrls) //appended to /app 
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
+
+const port = 4000
+app.listen(port, () => console.log("Server is up and running at port " + port))
 
 //connect application to a mongodb database
+
---
+++ signup-front/src/components/auth/SignUp.js
@@ -0,0 +1,129 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+// import { Link } from "react-router-dom";
+
+class SignUp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      password2: "",
+      errors: {},
+    };
+    // this.changeFullName = this.changeFullName.bind(this);
+    // this.changeEmail = this.changeEmail.bind(this);
+    // this.changePassword = this.changePassword.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  // changeFullName(event) {
+  //   this.setState({
+  //     name: event.target.value,
+  //   });
+  // }
+  // changeEmail(event) {
+  //   this.setState({
+  //     email: event.target.value,
+  //   });
+  // }
+  // changePassword(event) {
+  //   this.setState({
+  //     password: event.target.value,
+  //   });
+  // }
+
+  onChange = (e) => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
+
+  onSubmit(event) {
+    event.preventDefault();
+
+    const registered = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    };
+
+    axios
+      .post("http://localhost:4000/api/users/register", registered)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err.response.data));
+
+    //if we have hpage to go to
+    // window.location = '/'
+    this.setState({
+      name: "",
+      email: "",
+      password: "",
+      password2: "",
+    });
+  }
+
+  render() {
+    const { errors } = this.state;
+
+    return (
+      <div>
+        <div className="container">
+          <h4>
+            <b>Register</b> below
+          </h4>
+          <div className="form-div">
+            <form onSubmit={this.onSubmit}>
+              <input
+                type="text"
+                placeholder="Full Name"
+                onChange={this.onChange}
+                value={this.state.name}
+                id="name"
+                className="form-control form-group"
+              />
+
+              <input
+                type="text"
+                placeholder="E-mail"
+                onChange={this.onChange}
+                value={this.state.email}
+                id="email"
+                error={errors.email}
+                className="form-control form-group"
+              />
+
+              <input
+                type="password"
+                placeholder="Password"
+                onChange={this.onChange}
+                value={this.state.password}
+                id="password"
+                error={errors.password}
+                className="form-control form-group"
+              />
+
+              <input
+                type="password"
+                placeholder="Confirm password"
+                onChange={this.onChange}
+                value={this.state.password2}
+                error={errors.password2}
+                id="password2"
+                className="form-control form-group"
+              />
+
+              <input
+                type="submit"
+                className="btn btn-danger btn-block"
+                value="Submit"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SignUp;
