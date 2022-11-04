1fadc9f74bfea292dad82535f1bc6cb18e682129
Hien-Minh Le <minkdono@gmail.com>
2022-11-04 00:32:49 -0400

    completed running concurrently, react cannot run due to conflicts

--- server/auth/routes/api/users.js
+++ server/auth/routes/api/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 // Load input validation
 const validateRegisterInput = require("../../validation/register");
@@ -46,51 +46,51 @@ router.post("/register", (req, res) => {
 // @route POST api/users/login
 // @desc Login user and return JWT token
 // @access Public
-router.post("/login", (req, res) => {
-  // Form validation
-  const { errors, isValid } = validateLoginInput(req.body);
-  // Check validation
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-  const email = req.body.email;
-  const password = req.body.password;
-  // Find user by email
-  User.findOne({ email }).then((user) => {
-    // Check if user exists
-    if (!user) {
-      return res.status(404).json({ emailnotfound: "Email not found" });
-    }
-    // Check password
-    bcrypt.compare(password, user.password).then((isMatch) => {
-      if (isMatch) {
-        // User matched
-        // Create JWT Payload
-        const payload = {
-          id: user.id,
-          name: user.name,
-        };
-        // Sign token
-        jwt.sign(
-          payload,
-          keys.secretOrKey,
-          {
-            expiresIn: 12345, 
-          },
-          (err, token) => {
-            res.json({
-              success: true,
-              token: "Bearer " + token,
-            });
-          }
-        );
-      } else {
-        return res
-          .status(400)
-          .json({ passwordincorrect: "Password incorrect" });
-      }
-    });
-  });
-});
+// router.post("/login", (req, res) => {
+//   // Form validation
+//   const { errors, isValid } = validateLoginInput(req.body);
+//   // Check validation
+//   if (!isValid) {
+//     return res.status(400).json(errors);
+//   }
+//   const email = req.body.email;
+//   const password = req.body.password;
+//   // Find user by email
+//   User.findOne({ email }).then((user) => {
+//     // Check if user exists
+//     if (!user) {
+//       return res.status(404).json({ emailnotfound: "Email not found" });
+//     }
+//     // Check password
+//     bcrypt.compare(password, user.password).then((isMatch) => {
+//       if (isMatch) {
+//         // User matched
+//         // Create JWT Payload
+//         const payload = {
+//           id: user.id,
+//           name: user.name,
+//         };
+//         // Sign token
+//         jwt.sign(
+//           payload,
+//           keys.secretOrKey,
+//           {
+//             expiresIn: 12345, 
+//           },
+//           (err, token) => {
+//             res.json({
+//               success: true,
+//               token: "Bearer " + token,
+//             });
+//           }
+//         );
+//       } else {
+//         return res
+//           .status(400)
+//           .json({ passwordincorrect: "Password incorrect" });
+//       }
+//     });
+//   });
+// });
 
 module.exports = router;
--- server/auth/server.js
+++ server/auth/server.js
@@ -3,7 +3,7 @@ const app = express()
 const mongoose = require('mongoose')
 //const routesUrls = require('./routes/routes')
 const cors = require('cors')
-const passport = require("passport/lib");
+// const passport = require("passport/lib");
 const users = require("./routes/api/users");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
@@ -30,12 +30,12 @@ app.use(
 app.use(cors())
 //app.use('/app', routesUrls) //appended to /app 
 
-// Passport middleware
-app.use(passport.initialize());
-// Passport config
-require("./config/passport")(passport);
-// Routes
-app.use("/api/users", users);
+// // Passport middleware
+// app.use(passport.initialize());
+// // Passport config
+// require("./config/passport")(passport);
+// // Routes
+// app.use("/api/users", users);
 
 const port = 4000
 app.listen(port, () => console.log("Server is up and running at port " + port))
